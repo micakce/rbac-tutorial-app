017ba275b1b543820215c7874635371cd737a103
micakce <micakce@gmail.com>
2019-10-30 00:17:07 -0300

    'Using the Can component'

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "micakce.auth0.com",
+  roleUrl: "http://rbac-tutorial-app/role",
+  clienId: "39ZM1jegcqB1I6Lk0WXETp6403hkS4jb",
+  callbackUrl: "http://localhost:3000/callback"
+};
--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/home';
 import DashboardPage from './pages/dashboard';
+import CallbackPage from './pages/callback';
+import Auth from './components/Auth';
+
 
 function App () {
   return (
     <div className="App container">
-      <div className="jumbotron">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/dashboard" component={DashboardPage}/>
-          </Switch>
-        </Router>
-      </div>
+      <Auth>
+        <div className="jumbotron">
+          <Router>
+            <Switch>
+              <Route exact path="/" component={HomePage}/>
+              <Route path="/dashboard" component={DashboardPage}/>
+              <Route path="/callback" component={CallbackPage}/>
+            </Switch>
+          </Router>
+        </div>
+      </Auth>
     </div>
   );
 }
